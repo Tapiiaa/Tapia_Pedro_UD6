8150c5f86789280e24dd4cf3ac4a9ff557961bdb
Tapiiaa <ptapilob@myuax.com>
2024-02-25 10:53:50 +0100

    EJERCICIO 3 ERRORES CORREGIDOS

--- src/Ejercicio3/Analizador.java
+++ src/Ejercicio3/Analizador.java
@@ -1,15 +1,16 @@
 package Ejercicio3;
 
-public class CrearTablas {
+public class Analizador {
     public static void main(String[] args) {
+        //Creación de la tabla
         CreadorTablas creadorTablas = new CreadorTablas(10, 1, 100);
         int[] tabla = creadorTablas.crearTabla();
-
+        //Análisis de la tabla
         AnalisisMayor analisisMayor = new AnalisisMayor(tabla);
         int mayor = analisisMayor.getMayor();
-
+        for(int valor: tabla){
+            System.out.println(valor);
+        }
         System.out.println("El número mayor es: " + mayor);
-
-
     }
 }
--- src/Ejercicio3/CreadorTablas.java
+++ src/Ejercicio3/CreadorTablas.java
@@ -1,5 +1,7 @@
 package Ejercicio3;
 
+import java.util.Random;
+
 public class CreadorTablas {
     int tamano;
     int mínimo;
@@ -11,4 +13,14 @@ public class CreadorTablas {
         this.máximo = máximo;
     }
 
-}
+    public int[]crearTabla(){
+        int[] tabla = new int[tamano];
+        Random rnd = new Random();
+        for(int i = 0; i < tabla.length; i++){
+            tabla[i] = rnd.nextInt(máximo - mínimo + 1) + mínimo;
+        }
+        return tabla;
+    }
+    }
+
+
